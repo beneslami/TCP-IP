27d18b567697fcab8bb487a0544b0dbedd780028
Benyamin <beneslami72@gmail.com>
2022-07-18 16:21:52 +0200

    small change

--- testapp.c
+++ testapp.c
@@ -2,15 +2,25 @@
 // Created by Ben on 7/5/22.
 //
 #include "graph.h"
+#include "comm.h"
 #include "CommandParser/libcli.h"
+#include <stdio.h>
 
 graph_t *topo = NULL;
+
 extern graph_t *build_first_topo();
 extern void nw_init_cli();
 int
 main(int argc, char **argv){
     nw_init_cli();
     topo = build_first_topo();
+
+    //sleep(2);
+    node_t *snode = get_node_by_node_name(topo, "R0_re");
+    interface_t *oif = get_node_if_by_name(snode, "eth0/0");
+    char msg[] = "Hello. this is test\0";
+    send_pkt_out(msg, strlen(msg), oif);
+
     start_shell();
     return 0;
 }
