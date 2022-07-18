739c58ef5fc150247ba43b2053fa58da19085559
Benyamin <beneslami72@gmail.com>
2022-07-18 16:21:34 +0200

    send packet

---
+++ comm.h
@@ -0,0 +1,15 @@
+//
+// Created by Ben on 7/18/22.
+//
+
+#ifndef TCP_IP_COMM_H
+#define TCP_IP_COMM_H
+
+#define MAX_PACKET_BUFFER_SIZE  2048
+typedef struct node_ node_t;
+typedef struct interface_ interface_t;
+
+int send_pkt_out(char *pkt, unsigned int pkt_size, interface_t *interface);
+int pkt_receive(node_t *node, interface_t *interface,char *pkt, unsigned int pkt_size);
+
+#endif //TCP_IP_COMM_H
