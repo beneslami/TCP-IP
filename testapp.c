0a088a8c0ec82df5cb50034438ef384cd45ca8c4
Benyamin <beneslami72@gmail.com>
2022-07-21 21:40:06 +0200

    add topology

--- testapp.c
+++ testapp.c
@@ -9,17 +9,17 @@
 graph_t *topo = NULL;
 
 extern graph_t *build_first_topo();
+extern graph_t *build_linear_topo();
 extern void nw_init_cli();
-int
-main(int argc, char **argv){
-    nw_init_cli();
-    topo = build_first_topo();
 
-    //sleep(2);
-    node_t *snode = get_node_by_node_name(topo, "R0_re");
+int main(int argc, char **argv){
+    nw_init_cli();
+    topo = build_linear_topo();
+
+    /*node_t *snode = get_node_by_node_name(topo, "R0_re");
     interface_t *oif = get_node_if_by_name(snode, "eth0/0");
     char msg[] = "Hello. this is test\0";
-    send_pkt_out(msg, strlen(msg), oif);
+    send_pkt_out(msg, strlen(msg), oif);*/
 
     start_shell();
     return 0;
--- topologies.c
+++ topologies.c
@@ -6,6 +6,7 @@
 #include "net.h"
 
 extern void network_start_pkt_receiver_thread(graph_t *topo);
+extern void interface_assign_mac_address(interface_t *interface);
 
 graph_t *build_first_topo(){
     graph_t *topo = create_new_graph("Generic graph");
@@ -28,6 +29,28 @@ graph_t *build_first_topo(){
     node_set_loopback_address(R2_re, "122.1.1.2");
     node_set_intf_ip_address(R2_re, "eth0/3", "30.1.1.2", 24);
     node_set_intf_ip_address(R2_re, "eth0/5", "40.1.1.2", 24);
+
+    network_start_pkt_receiver_thread(topo);
+    return topo;
+}
+
+graph_t *build_linear_topo(){
+    graph_t *topo = create_new_graph("Linear topology");
+    node_t *R1 = create_graph_node(topo, "R1");
+    node_t *R2 = create_graph_node(topo, "R2");
+    node_t *R3 = create_graph_node(topo, "R3");
+
+    insert_link_between_two_nodes(R1, R2, "eth0/1", "eth0/2", 1);
+    insert_link_between_two_nodes(R2, R3, "eth0/3", "eth0/4", 1);
+
+    node_set_loopback_address(R1, "122.1.1.1");
+    node_set_loopback_address(R2, "122.1.1.2");
+    node_set_loopback_address(R3, "122.1.1.3");
+
+    node_set_intf_ip_address(R1, "eth0/1", "10.1.1.1", 24);
+    node_set_intf_ip_address(R2, "eth0/2", "10.1.1.2", 24);
+    node_set_intf_ip_address(R2, "eth0/3", "20.1.1.2", 24);
+    node_set_intf_ip_address(R3, "eth0/4", "20.1.1.1", 24);
 
     network_start_pkt_receiver_thread(topo);
     return topo;
