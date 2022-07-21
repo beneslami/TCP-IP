3e208f5335a3708d634e9e7729edcb0a11792629
Benyamin <beneslami72@gmail.com>
2022-07-21 21:40:25 +0200

    L2 broadcast

--- utils.h
+++ utils.h
@@ -10,4 +10,9 @@ typedef enum{
     TRUE
 } bool_t;
 
+#define IS_MAC_BROADCAST_ADDR(mac)  \
+    (mac[0] == 0xFF  &&  mac[1] == 0xFF && mac[2] == 0xFF && \
+     mac[3] == 0xFF  &&  mac[4] == 0xFF && mac[5] == 0xFF)
+
+void layer2_fill_with_broadcast_mac(char *mac_array);
 #endif //TCP_IP_UTILS_H
