5089d7a1a39e271da407b0a509ca10be034dbdf5
Benyamin <beneslami72@gmail.com>
2022-07-26 17:10:33 +0200

    helper functions

--- utils.h
+++ utils.h
@@ -5,6 +5,8 @@
 #ifndef TCP_IP_UTILS_H
 #define TCP_IP_UTILS_H
 
+#include <stdint.h>
+
 typedef enum{
     FALSE,
     TRUE
@@ -15,4 +17,8 @@ typedef enum{
      mac[3] == 0xFF  &&  mac[4] == 0xFF && mac[5] == 0xFF)
 
 void layer2_fill_with_broadcast_mac(char *mac_array);
+void apply_mask(char *prefix, char mask, char *str_prefix);
+char *tcp_ip_covert_ip_n_to_p(uint32_t ip_addr, char *output_buffer);
+uint32_t tcp_ip_covert_ip_p_to_n(char *ip_addr);
+
 #endif //TCP_IP_UTILS_H
