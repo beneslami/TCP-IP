8ffa408175d74db42ee465f450b95a70550709a6
Benyamin <beneslami72@gmail.com>
2022-07-26 17:10:19 +0200

    ARP front end still buggy

--- nwcli.c
+++ nwcli.c
@@ -22,6 +22,18 @@ static int show_nw_topology_handler(param_t *param, ser_buff_t *tlv_buf, op_mode
     }
 }
 
+void display_graph_nodes(param_t *param, ser_buff_t *tlv_buf){
+
+    node_t *node;
+    glthread_t *curr;
+
+    ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr){
+
+        node = graph_glue_to_node(curr);
+        printf("%s\n", node->node_name);
+    } ITERATE_GLTHREAD_END(&topo->node_list, curr);
+}
+
 void nw_init_cli(){
     init_libcli();
     param_t *show = libcli_get_show_hook();
@@ -32,5 +44,24 @@ void nw_init_cli(){
         libcli_register_param(show, &topology);
         set_param_cmd_code(&topology, CMDCODE_SHOW_NW_TOPOLOGY);
     }
+
     support_cmd_negation(config);
+}
+
+extern void send_arp_broadcast_request(node_t *node, interface_t *oif, char *ip_addr);
+
+static int arp_handler(param_t *param, ser_buff_t *tlv_buf, op_mode enable_or_disable){
+    node_t *node;
+    char *node_name;
+    char *ip_addr;
+    tlv_struct_t *tlv = NULL;
+    TLV_LOOP_BEGIN(tlv_buf, tlv){
+        if(strncmp(tlv->leaf_id, "node-name", strlen("node-name")) == 0)
+            node_name = tlv->value;
+        else if(strncmp(tlv->leaf_id, "ip-address", strlen("ip-address")) == 0)
+            ip_addr = tlv->value;
+    }TLV_LOOP_END
+    node = get_node_by_node_name(topo, node_name);
+    send_arp_broadcast_request(node, NULL, ip_addr);
+    return 0;
 }
---
+++ tcpconst.h
@@ -0,0 +1,13 @@
+//
+// Created by Ben on 7/22/22.
+//
+
+#ifndef TCP_IP_TCPCONST_H
+#define TCP_IP_TCPCONST_H
+
+#define ARP_BROAD_REQ   1
+#define ARP_REPLY       2
+#define ARP_MSG         806
+#define BROADCAST_MAC   0xFFFFFFFFFFFF
+
+#endif
